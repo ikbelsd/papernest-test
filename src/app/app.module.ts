bb28377b1e937fcf22214ce98daa077c9f064b6d
ikbelsd <ikbel.sdiri@featway.fr>
2024-06-19 15:27:42 +0200

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { TodoSearchComponent } from './components/todo-search/todo-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoInputComponent,
+    TodoSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/todo-search/todo-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-search',
+  templateUrl: './todo-search.component.html',
+  styleUrl: './todo-search.component.scss'
+})
+export class TodoSearchComponent {
+
+}
